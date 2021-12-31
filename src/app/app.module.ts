cf90e9e1870bf164beec887b00414f7fb30e9c47
Aron Teh <aronteh.chess@gmail.com>
2021-12-31 23:07:31 +0000

    Threw out the mat-card, using native div to represent a card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { ToastrModule } from 'ngx-toastr';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
